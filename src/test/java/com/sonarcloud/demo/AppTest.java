ddfb26351c0c563770bed67ee54bb24c33a552d0
khilendra <khilook@gmail.com>
2025-11-08 18:39:28 +0530

    Add unit test for App class add method

---
+++ src/test/java/com/sonarcloud/demo/AppTest.java
@@ -0,0 +1,13 @@
+package com.sonarcloud.demo;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest {
+
+    @Test
+    void testAdd() {
+        App app = new App();
+        assertEquals(5, app.add(2, 3));
+    }
+}
