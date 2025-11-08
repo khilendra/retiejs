275c7ca48b17c317ffa8cfd102c6b613c531544a
khilendra <khilook@gmail.com>
2025-11-08 18:37:48 +0530

    Create App.java with main method and add function

---
+++ src/main/java/com/sonarcloud/demo/App.java
@@ -0,0 +1,11 @@
+package com.sonarcloud.demo;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello from SonarCloud sample!");
+    }
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
